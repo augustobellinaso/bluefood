0b600d652c88a73ab2dc6ab5f36f3439ed72099f
Augusto Loose Bellinaso <augustobellinaso@gmail.com>
2021-03-29 19:59:24 -0300

    Criando as primeiras entidades

---
+++ src/main/java/augustobellinaso/bluefood/domain/cliente/Cliente.java
@@ -0,0 +1,11 @@
+package augustobellinaso.bluefood.domain.cliente;
+
+import augustobellinaso.bluefood.domain.usuario.Usuario;
+
+public class Cliente extends Usuario {
+
+
+    private String cpf;
+
+    private String cep;
+}
---
+++ src/main/java/augustobellinaso/bluefood/domain/usuario/Usuario.java
@@ -0,0 +1,14 @@
+package augustobellinaso.bluefood.domain.usuario;
+
+public class Usuario {
+
+    private Integer id;
+
+    private String nome;
+
+    private String email;
+
+    private String telefone;
+
+    private String senha;
+}
