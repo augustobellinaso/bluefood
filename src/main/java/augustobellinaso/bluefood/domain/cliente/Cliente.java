7e498f28e3d7922348fc250ab6d50c9926e60e86
Augusto Loose Bellinaso <augustobellinaso@gmail.com>
2021-03-29 20:29:38 -0300

    Adicionado as regras de validação no domínio

--- src/main/java/augustobellinaso/bluefood/domain/cliente/Cliente.java
+++ src/main/java/augustobellinaso/bluefood/domain/cliente/Cliente.java
@@ -5,7 +5,10 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 @Getter
 @Setter
@@ -13,7 +16,12 @@ import javax.persistence.Entity;
 @Entity
 public class Cliente extends Usuario {
 
+    @NotBlank(message = "O CPF não pode estar vazio")
+    @Pattern(regexp = "[0-9]{11}", message = "O CPF possui formato inválido")
+    @Column(length = 11, nullable = false)
     private String cpf;
 
+    @NotBlank(message = "O CEP não pode estar vazio")
+    @Pattern(regexp = "[0-9]{8}", message = "O CEP possui formato inválido")
     private String cep;
 }
