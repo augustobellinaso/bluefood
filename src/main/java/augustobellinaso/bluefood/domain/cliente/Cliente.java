8881900d4340cd32faa32f1bc6f32dd2410648fa
Augusto Loose Bellinaso <augustobellinaso@gmail.com>
2021-03-29 20:02:03 -0300

    Adicionando anotações
    Adiciondo Getters e Setters. EqualsAndHashCode. Declaração de Entidade e a chave primária.

--- src/main/java/augustobellinaso/bluefood/domain/cliente/Cliente.java
+++ src/main/java/augustobellinaso/bluefood/domain/cliente/Cliente.java
@@ -1,9 +1,17 @@
 package augustobellinaso.bluefood.domain.cliente;
 
 import augustobellinaso.bluefood.domain.usuario.Usuario;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
+import javax.persistence.Entity;
+
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true, callSuper = true)
+@Entity
 public class Cliente extends Usuario {
-
 
     private String cpf;
 
--- src/main/java/augustobellinaso/bluefood/domain/usuario/Usuario.java
+++ src/main/java/augustobellinaso/bluefood/domain/usuario/Usuario.java
@@ -1,7 +1,21 @@
 package augustobellinaso.bluefood.domain.usuario;
 
-public class Usuario {
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.io.Serializable;
+
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@MappedSuperclass
+public class Usuario implements Serializable {
+
+    @EqualsAndHashCode.Include
+    @Id
     private Integer id;
 
     private String nome;
