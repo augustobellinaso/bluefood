b1e382ee60cb0b686837d78ec4f8b843723259a2
Augusto Loose Bellinaso <augustobellinaso@gmail.com>
2021-03-30 10:47:07 -0300

    Processando a validação de informações

--- src/main/java/augustobellinaso/bluefood/infrastructure/web/controller/PublicController.java
+++ src/main/java/augustobellinaso/bluefood/infrastructure/web/controller/PublicController.java
@@ -5,10 +5,13 @@ import augustobellinaso.bluefood.domain.cliente.Cliente;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping(path = "/public")
@@ -25,9 +28,15 @@ public class PublicController {
     }
 
     @PostMapping(path = "/cliente/save")
-    public String saveCliente(@ModelAttribute("cliente") Cliente cliente){
-        clienteService.saveCliente(cliente);
+    public String saveCliente(@ModelAttribute("cliente") @Valid Cliente cliente,
+                            Errors errors, Model model){
 
+        if (!errors.hasErrors()){
+            clienteService.saveCliente(cliente);
+            model.addAttribute("msg", "Cliente cadastrado com sucesso!");
+        }
+
+        ControllerHelper.setEditMode(model, false);
         return "cliente-cadastro";
     }
 }
