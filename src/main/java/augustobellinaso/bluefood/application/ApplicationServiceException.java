327351a5d76576901be33ad97993ca07008ab204
Augusto Loose Bellinaso <augustobellinaso@gmail.com>
2021-03-30 17:18:00 -0300

    Criando o serviço da imagem
    Método para upload do arquivo. Configurando o local para armazenar as imagens no arquivo.properties. Criando a classe ApplicationServiceException para lançar exceções.

---
+++ src/main/java/augustobellinaso/bluefood/application/ApplicationServiceException.java
@@ -0,0 +1,16 @@
+package augustobellinaso.bluefood.application;
+
+public class ApplicationServiceException extends RuntimeException {
+
+    public ApplicationServiceException(String message) {
+        super(message);
+    }
+
+    public ApplicationServiceException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ApplicationServiceException(Throwable cause) {
+        super(cause);
+    }
+}
