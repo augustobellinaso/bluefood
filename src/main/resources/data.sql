6404fe9bf3f5203fb06487e103cb2d54539528a8
Augusto Loose Bellinaso <augustobellinaso@gmail.com>
2021-03-30 14:31:33 -0300

    Criando o arquivo data.sql
    Arquivo com os comandos em SQL para fazer a inserção das categorias de comida na respectiva tabela do banco de dados

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO categoria_restaurante (nome, imagem) VALUES ('Pizza', '0001_categoria.png');
+INSERT INTO categoria_restaurante (nome, imagem) VALUES ('Sanduíche', '0002_categoria.png');
+INSERT INTO categoria_restaurante (nome, imagem) VALUES ('Churrasco', '0003_categoria.png');
+INSERT INTO categoria_restaurante (nome, imagem) VALUES ('Salada', '0004_categoria.png');
+INSERT INTO categoria_restaurante (nome, imagem) VALUES ('Sobremesa', '0005_categoria.png');
+INSERT INTO categoria_restaurante (nome, imagem) VALUES ('Japonês', '0006_categoria.png');
