8dda0700a12fcf6752695c206a283d7940c8a800
Augusto Loose Bellinaso <augustobellinaso@gmail.com>
2021-04-01 10:47:56 -0300

    Inserindo dados na classe de teste
    E criando o repositório para os itens de cardápio

---
+++ src/main/java/augustobellinaso/bluefood/domain/restaurante/ItemCardapioRepository.java
@@ -0,0 +1,6 @@
+package augustobellinaso.bluefood.domain.restaurante;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemCardapioRepository extends JpaRepository<ItemCardapio, Integer> {
+}
