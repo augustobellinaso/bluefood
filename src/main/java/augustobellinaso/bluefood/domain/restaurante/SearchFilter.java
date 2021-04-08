adaa555c7314701fe6959ab1d1da8ed899a3743e
Augusto Loose Bellinaso <augustobellinaso@gmail.com>
2021-04-08 15:34:02 -0300

    Fazendo a parte de buscas por categorias
    Criando um Enum para verificar qual tipo de busca vamos fazer (texto ou por categoria). Criando uma função em JavaScript para identificar o tipo de pesquisa e submeter o formulário conforme o tipo passado.

--- src/main/java/augustobellinaso/bluefood/domain/restaurante/SearchFilter.java
+++ src/main/java/augustobellinaso/bluefood/domain/restaurante/SearchFilter.java
@@ -5,5 +5,11 @@ import lombok.Data;
 @Data
 public class SearchFilter {
 
+    public enum SearchType {
+        Texto, Categoria
+    }
+
     private String texto;
+    private SearchType searchType;
+    private Integer categoriaId;
 }
