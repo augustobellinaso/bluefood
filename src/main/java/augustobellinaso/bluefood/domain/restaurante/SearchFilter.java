35e4d2aac98146366b1359fff419c88e77537aa8
Augusto Loose Bellinaso <augustobellinaso@gmail.com>
2021-04-08 16:17:35 -0300

    Iniciando a parte de filtrar os resultados das pesquisas
    Criando o enum com os tipos de filtros. Criando a flag para ordenamento ascendente ou descendente e para entrega grátis ou não. Criando Enum para saber qual opção foi escolhida.

--- src/main/java/augustobellinaso/bluefood/domain/restaurante/SearchFilter.java
+++ src/main/java/augustobellinaso/bluefood/domain/restaurante/SearchFilter.java
@@ -6,12 +6,23 @@ import lombok.Data;
 public class SearchFilter {
 
     public enum SearchType {
-        Texto, Categoria
+        Texto, Categoria;
+    }
+
+    public enum Order {
+        Taxa, Tempo;
+    }
+
+    public enum Command {
+        EntregaGratis, MaiorTaxa, MenorTaxa, MaiorTempo, MenorTempo;
     }
 
     private String texto;
     private SearchType searchType;
     private Integer categoriaId;
+    private Order order = Order.Taxa;
+    private boolean asc;
+    private boolean entregaGratis;
 
     public void processFilter() {
 
