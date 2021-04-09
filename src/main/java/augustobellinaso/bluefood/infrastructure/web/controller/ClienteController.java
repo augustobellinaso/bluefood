592dbb5e3ae64e71470dfa7ab4c200e6779704a3
Augusto Loose Bellinaso <augustobellinaso@gmail.com>
2021-04-09 12:28:48 -0300

    Configurando a parte dos filtros nos restaurantes
    Realizando a busca com categoria e sem categoria. Função JavaScript para configurar os botões de filtro das categorias do restaurante. Finalizando a parte dos filtros dos itens de cardápio dos restaurantes

--- src/main/java/augustobellinaso/bluefood/infrastructure/web/controller/ClienteController.java
+++ src/main/java/augustobellinaso/bluefood/infrastructure/web/controller/ClienteController.java
@@ -94,6 +94,7 @@ public class ClienteController {
 
     @GetMapping(path = "/restaurante")
     public String viewRestaurante(@RequestParam("restauranteId") Integer restauranteId,
+                                  @RequestParam(value = "categoria", required = false) String categoria,
                                   Model model){
 
         Restaurante restaurante = restauranteRepository.findById(restauranteId).orElseThrow();
@@ -106,15 +107,18 @@ public class ClienteController {
         List<ItemCardapio> itensCardapioDestaque;
         List<ItemCardapio> itensCardapioNaoDestaque;
 
-        itensCardapioDestaque = itemCardapioRepository.findByRestaurante_IdAndDestaqueOrderByNome(restauranteId, true);
+        if (categoria == null) {
+            itensCardapioDestaque = itemCardapioRepository.findByRestaurante_IdAndDestaqueOrderByNome(restauranteId, true);
+            itensCardapioNaoDestaque = itemCardapioRepository.findByRestaurante_IdAndDestaqueOrderByNome(restauranteId, false);
+
+        } else {
+            itensCardapioDestaque = itemCardapioRepository.findByRestaurante_IdAndDestaqueAndCategoriaOrderByNome(restauranteId, true, categoria);
+            itensCardapioNaoDestaque = itemCardapioRepository.findByRestaurante_IdAndDestaqueAndCategoriaOrderByNome(restauranteId, false, categoria);
+        }
+
         model.addAttribute("itensCardapioDestaque", itensCardapioDestaque);
-
-        itensCardapioNaoDestaque = itemCardapioRepository.findByRestaurante_IdAndDestaqueOrderByNome(restauranteId, false);
         model.addAttribute("itensCardapioNaoDestaque", itensCardapioNaoDestaque);
-
-
-
-
+        model.addAttribute("categoriaSelecionada", categoria);
 
         return "cliente-restaurante";
     }
