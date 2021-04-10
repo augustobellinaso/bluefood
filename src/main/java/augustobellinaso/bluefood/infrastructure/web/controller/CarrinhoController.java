93b739f6f2e02503cf1283ad11a4b28aa2568121
Augusto Loose Bellinaso <augustobellinaso@gmail.com>
2021-04-10 15:33:03 -0300

    Criando o controller do carrinho
    Fazendo a página do carrinho do cliente. Implementando o método para criar o carrinho e adicionar itens nele. O método de criar o carrinho funcioan de modo a fazer com que ele permaneça aberto durante a sessão do usuário que está logado.

---
+++ src/main/java/augustobellinaso/bluefood/infrastructure/web/controller/CarrinhoController.java
@@ -0,0 +1,43 @@
+package augustobellinaso.bluefood.infrastructure.web.controller;
+
+import augustobellinaso.bluefood.domain.pedido.Carrinho;
+import augustobellinaso.bluefood.domain.pedido.RestauranteDiferenteException;
+import augustobellinaso.bluefood.domain.restaurante.ItemCardapio;
+import augustobellinaso.bluefood.domain.restaurante.ItemCardapioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/cliente/carrinho")
+@SessionAttributes("carrinho")
+public class CarrinhoController {
+
+    @Autowired
+    private ItemCardapioRepository itemCardapioRepository;
+
+    @ModelAttribute("carrinho")
+    public Carrinho carrinho() {
+        return new Carrinho();
+    }
+
+    @GetMapping(path = "/adicionar")
+    public String adicionarItem(
+            @RequestParam("itemId") Integer itemId,
+            @RequestParam("quantidade") Integer quantidade,
+            @RequestParam("observacoes") String observacoes,
+            @ModelAttribute("carrinho") Carrinho carrinho,
+            Model model) {
+
+        ItemCardapio itemCardapio = itemCardapioRepository.findById(itemId).orElseThrow();
+
+        try {
+            carrinho.adicionarItem(itemCardapio, quantidade, observacoes);
+        } catch (RestauranteDiferenteException e) {
+            model.addAttribute("msg", "Não é possível misturar comidas de restaurantes diferentes!");
+        }
+
+        return "cliente-carrinho";
+    }
+}
