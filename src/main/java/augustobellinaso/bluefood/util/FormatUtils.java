12b0349f38a8f580fd49151389f39ce8c422c900
Augusto Loose Bellinaso <augustobellinaso@gmail.com>
2021-04-14 15:05:03 -0300

    Resolvendo bug no cadastro de restaurante
    Problema ao cadastrar taxa de entrega com valores em decimal (3,50). Criando uma classe utilitária para fazer formatação. Criando método para formatar números utilizando critérios de localidade. Criando método para formatar BigDecimal para String.

---
+++ src/main/java/augustobellinaso/bluefood/util/FormatUtils.java
@@ -0,0 +1,28 @@
+package augustobellinaso.bluefood.util;
+
+import java.math.BigDecimal;
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class FormatUtils {
+
+    private static final Locale LOCALE_BRAZIL = new Locale("pt", "BR");
+
+    public static NumberFormat newCurrencyFormat() {
+        NumberFormat nf = NumberFormat.getCurrencyInstance(LOCALE_BRAZIL);
+
+        nf.setMaximumFractionDigits(2);
+        nf.setMinimumFractionDigits(2);
+        nf.setGroupingUsed(false); //não vai usar separadores de milhar
+
+        return nf;
+    }
+
+    public static String formatCurrency(BigDecimal number) {
+        if (number == null) {
+            return null;
+        }
+
+        return newCurrencyFormat().format(number);
+    }
+}
